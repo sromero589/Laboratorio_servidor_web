c80274195ccc0b7771f8a27859f1f655af33bdab
sromero589 <35242455+sromero589@users.noreply.github.com>
2018-01-16 01:59:10 +0000

    Update Documento.php
    Complementando comentarios

--- modelo/Documento.php
+++ modelo/Documento.php
@@ -1,19 +1,11 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Clase Documento
  *
- * @author casa
  */
 class Documento {
 
-    //put your code here
     public $codigo;
     public $titulo;
     public $lugar;
@@ -25,7 +17,7 @@ class Documento {
     public $mysqli;
 
     /**
-     * Constructor de la clase, genera conexión base de datos.
+     * Constructor de la clase, genera la conexión con la base de datos.
      */
     public function __construct() {
 
