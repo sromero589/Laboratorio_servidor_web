4a231a7f63502dba9c541a215322ad416dd29963
sromero589 <35242455+sromero589@users.noreply.github.com>
2018-01-16 02:27:24 +0000

    Update redirec.php

--- redirec.php
+++ redirec.php
@@ -5,22 +5,22 @@ require_once 'modelo/basededatos.php';
 
 $controller = $_GET["c"];
 
-// Todo esta lógica hará el papel de un FrontController
+// Hace el papel de un FrontController
 if (!isset($_REQUEST['c'])) {
     require_once "controlador/$controller.controlador.php";
     $controller = ucwords($controller) . 'Controlador';
     $controller = new $controller;
     $controller->Index();
 } else {
-    // Obtenemos el controlador que queremos cargar
+    // Se obtiene el controlador que se quiere cargar
     $controller = strtolower($_REQUEST['c']);
     $accion = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'Index';
 
-    // Instanciamos el controlador
+    // Se instancia el controlador
     require_once "controlador/$controller.controlador.php";
     $controller = ucwords($controller) . 'Controlador';
     $controller = new $controller;
 
-    // Llama la accion
+    // Se llama a la acción
     call_user_func(array($controller, $accion));
 }
